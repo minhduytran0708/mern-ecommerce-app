19479b776b827431765b96bef4319c839cec142b
LAPTOP-B1S5PKP9\Minh Di <duytm@cnv.vn>
2022-04-16 20:07:37 +0700

    up

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const stripe = requrie("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 
 router.post("/payment", (req, res) => {
